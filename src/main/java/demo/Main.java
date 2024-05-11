9a4385858d20de6ca12b0bbcfa6b0d341568c6fc
Harsh Pashine <pashineharsh01@gmail.com>
2024-05-11 11:04:28 +0530

    added some more functions

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,6 +1,9 @@
 package demo;
 
 public class Main {
+    public void show() {
+        System.out.println("Hello!!");
+    }
     public int add(int a, int b) {
         return a+b;
     }
@@ -15,6 +18,7 @@ public class Main {
     }
     public static void main(String[] args) {
         Main m = new Main();
+        m.show();
         System.out.println("addition is: "+m.add(5, 10));
         System.out.println("Subtraction is: "+m.sub(5, 10));
         System.out.println("multiplication is: "+m.mul(5, 10));
